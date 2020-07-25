081de0d289cda7eb116aa359994de6bbfa7ba6f1
Nico Esposito <nico.esposito@gmail.com>
2020-07-24 22:26:17 -0300

    Create images component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_PIXABAY_KEY);
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .then((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="maw-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 tex-txl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            2000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            4000
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 rounded-full">
-          #tag
-        </span>
-        <span className="inline-block bg-gray-200 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 rounded-full">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounder-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 rounded-full">
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
